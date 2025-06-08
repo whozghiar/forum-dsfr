e0c03645934c0c48425c4108868dc74c7bdb3594
whozghiar <theolomege@gmail.com>
2025-06-08 20:01:53 +0200

    - Ajout d'un Keycloak - Modification de la classe SecurityConfig.java - Ajout d'instructions dans le docker-compose.yml - Export du "realm" keycloak

--- backend_forum/src/main/java/fr/dsfr/forum/configurations/SecurityConfig.java
+++ backend_forum/src/main/java/fr/dsfr/forum/configurations/SecurityConfig.java
@@ -5,12 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -32,7 +38,10 @@ public class SecurityConfig {
         http
                 // Autorise toutes les requêtes sans restriction
                 .authorizeHttpRequests(authz -> authz
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
                 )
                 // Configure les règles CORS en utilisant une source de configuration
                 .cors(cors -> cors
@@ -78,4 +87,19 @@ public class SecurityConfig {
             // Retourne la source de configuration CORS
             return source;
         }
+
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+            if (realmAccess == null || !realmAccess.containsKey("roles")) {
+                return Collections.emptyList();
+            }
+            Collection<String> roles = (Collection<String>) realmAccess.get("roles");
+            return roles.stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .collect(Collectors.toSet());
+        });
+        return converter;
+    }
 }
